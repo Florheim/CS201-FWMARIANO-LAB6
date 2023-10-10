8a5581381670c66d2227200b1eec8743bb775202
Florheim <fwizard0719@gmail.com>
2023-10-10 19:50:17 +0800

    feat: added to html my javascript code

---
+++ Activity6.js
@@ -0,0 +1,7 @@
+let yourNameToCount = 10;
+let name = prompt ("Enter your name for counting to 100");
+
+while (yourNameToCount < 101) {
+    console.log (name, yourNameToCount);
+    yourNameToCount++;
+}
